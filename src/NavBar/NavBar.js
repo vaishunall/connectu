29ff1a44fbbc1d64d68e0f211a4f32349cb874b6
vainall <vaishunalla@yahoo.com>
2022-11-12 20:44:15 -0600

    added fonts and basic navbar

---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavBar.css'
+
+export default function NavBar() {
+    return (
+        <nav>
+          <ul className="list">
+              <li className="items">dashboard</li>
+              <li className="items">profile</li>
+              <li className="items">log in</li>
+            </ul>
+        </nav>
+      )
+   
+  
+}
