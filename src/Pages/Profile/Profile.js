c5c49eaf53dd6c35db08d1ec426a9f84fde8a903
Faiza Rahman <faizarahman301@gmail.com>
2022-11-13 02:18:26 -0600

    incomplete profile page

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -11,30 +11,22 @@ export default class Profile extends Component {
         </header>
         <main>
         <div className="card" >
-            <div className="cardContents">
-                <div className="cardMain">
-                <div profilePicture="picture"></div>
-                {/* <img className="eventImage" src={cardImage1}></img> */}
-                <div className="cardBody">
-                    <div className="cardHeader">Firstname Lastname</div>
-                    <div className="cardDetails">
-                    <div className="name">email</div>
-                    <div className="university">phone</div>
-                    <div className="club">bio</div>
-                    <div className="club">institution name</div>
-                    <div className="club">website</div>
-                    </div>
-                </div>
+            
                 
-                </div>
-                <div className="cardDescription">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis </p>  
-                </div>
-                <div className="cardTags">
-                <div className="tag"><div className="tagText">diversity</div></div>
-                <div className="tag"><div className="tagText">inclusion</div></div>
-                </div>
-            </div> 
+                {/* <img className="eventImage" src={cardImage1}></img> */}
+                <div className="cardTop">
+                  <div className="profilePicture"></div>
+                    <div className="userName">Faiza Rahman</div>
+                 </div>   
+                 <div className="cardBottom">
+                    <div className="email">Email</div>
+                    <div className="userEmail">example@gmail.com</div>
+                    <div className="phone">Phone</div>
+                    <div className="institution">Institution Name</div>
+                    <div className="website">Website</div>
+                    <div className="bio">Bio</div>
+                </div> 
+               
         </div>
         </main>
       </div>
