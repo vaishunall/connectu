2763015f22fdf0579481d80aa929a178a1b31483
vainall <vaishunalla@yahoo.com>
2022-11-13 00:13:21 -0600

    changes

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './Dashboard.css';
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div>Dashboard</div>
+      <div>
+        <div>
+            <h1 className='Heading' style={{textAlign: 'left', marginLeft: "7.5%"}}>top picks for you</h1>
+        </div>
+       
+      </div>
     )
   }
 }
--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,9 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import "./LoginPage.css";
+import bookImage from '../../Assets/books.png';
 
-export default class LoginPage extends Component {
-  render() {
-    return (
-      <div>LoginPage</div>
-    )
-  }
-}
+    export default class LoginPage extends Component {
+    render() {
+        return (
+        <div>
+            <div className='login-section'>
+                <div className="login-left">
+                    <img className="login-image" src={bookImage}></img>
+                </div>
+
+                <div className='login-right'>
+                    <div>
+                        <h1 className='heading'>login</h1>
+                        <form className="form" onSubmit={this.handleSubmit}>
+                            <div className="input-group">
+                                <input type="email" name="email" placeholder="email address" />
+                            </div>
+                            <div className="input-group">
+                            <input type="password" name="password" placeholder="password" />
+                        </div>
+                        <div className='button-login'>login</div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        )
+    }
+    }
