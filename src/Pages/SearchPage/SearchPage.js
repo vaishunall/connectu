28b463dbc6715fe309500051d3064b799f370a73
Palak Bhargava <palak_bhargava@hotmail.com>
2022-11-13 03:40:01 -0600

    search tags functionality

--- src/Pages/SearchPage/SearchPage.js
+++ src/Pages/SearchPage/SearchPage.js
@@ -2,15 +2,21 @@ import React, { Component, useState } from 'react'
 import "./SearchPage.css"
 import { Multiselect } from 'multiselect-react-dropdown';
 import SearchCard from '../../Components/Search-card/Search-card'
+import { Tags } from './Tags';
+import { Objects } from './Objects';
 
 
 export default function SearchPage() {
-    const [temp, setTemp] = useState(["A", "B", "C"]);
+    const [temp, setTemp] = useState([]);
 
+    const [selectedTags, setSelectedTags] = useState([]);
+
+    Tags.forEach(item => {
+        selectedTags.push(item.tag_name);
+      });
+    
     return (
         <div>
-
-
             <div className="search-input-div">
                 <div>
                     <input className="search-input" placeholder="Search an event" />
@@ -21,22 +27,29 @@ export default function SearchPage() {
                 <div className="tag-input">
                     <Multiselect
                         isObject={false}
-                        options={temp}
+                        options={selectedTags}
                         onRemove={(event) => console.log(event)}
                         onSelect={(event) => console.log(event)}
                         placeholder={"Enter Search Tag"}
-                        style={{ chips: { background: "red" }, searchBox: { border: 0 } }}
+                        style={{ chips: { background: "#D26E63" }, searchBox: { border: 0 } }}
                     />
                 </div>
             </div>
+            
             <div className="results-div">
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
-                <SearchCard className="card" data={{header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                {Objects.forEach(i => {
+                    if(selectedTags.some(r=> i.tags.includes(r))){
+                        <SearchCard className="card" data={i.tags, i.header, i.name, i.uni, i.club}/>
+                    }
+                    
+              })}
+                {/* <SearchCard className="card" data={{tags:["Diversity", "Inclusivity", "Women"], header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Diversity", "Inclusivity"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Minority"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Minority", "Inclusivity"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Diversity", "LGBTQ+"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Inclusivity", "Cloud"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/>
+                <SearchCard className="card" data={{tags:["Diversity", "ML"],header:"Women in Tech",name:"Faiza", uni:"The University of Texas at Dallas", club:"Society of Women Engineers", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc fermentum sapien sed aliquet ultricies. Nulla at risus sit amet libero semper sodales. Duis molestie sollicitudin neque, non commodo neque porttitor ut. Nullam gravida, turpis"}}/> */}
 
             </div>
         </div>
