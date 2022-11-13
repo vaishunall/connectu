b1434a947d0062a6fd2bd5ac3d7c9e78919dc544
Faiza Rahman <faizarahman301@gmail.com>
2022-11-13 02:41:04 -0600

    added search card component

---
+++ src/Components/Search-card/Search-card.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import "./Search-card.css"
+import cardImage1 from "../../Assets/nt-bill 1.png"
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
+
+export default function SearchCard({data}) {
+    return (
+      <div>
+        <header className="main-header">
+        <nav className="nav">
+        </nav>
+        </header>
+        <main>
+        <div className="card" >
+            <div className="cardContents">
+                <div className="cardMain">
+                
+                <img className="eventImage" src={cardImage1}></img>
+                <div className="cardBody">
+
+                    <div className="cardHeader">{data.header}</div>
+                    <div className="cardDetails">
+                    <div className="name">{data.name}</div>
+                    <div className="university">{data.uni}</div>
+                    <div className="club">{data.club}</div></div>
+                </div>
+                <div className="starIcon">
+                <StarOutlineIcon></StarOutlineIcon></div>
+                </div>
+        
+                <div className="cardTags">
+                <div className="tag"><div className="tagText">diversity</div></div>
+                <div className="tag"><div className="tagText">inclusion</div></div>
+                </div>
+            </div> 
+        </div>
+        </main>
+      </div>
+    )
+}
